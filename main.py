bbb50027bdfcf88fa664f33b68b5090ee436b552
monkeycompiler <sebastian.mulholland@outlook.co.nz>
2025-09-17 20:57:31 +1200

    made main() more readable

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 def get_book_text(path_to_file):
-    with open(path_to_file, 'r', encoding="utf-8") as f:
+    with open(path_to_file,) as f:
         return f.read()
 
 def main():
-    print(get_book_text('books/frankenstein.txt'))
+    book_path = "books/frankenstein.txt"
+    print(get_book_text(book_path))
 
 main()
