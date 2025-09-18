71f933c33e1a971213d759d7825c5e81c558016f
monkeycompiler <sebastian.mulholland@outlook.co.nz>
2025-09-18 21:09:09 +1200

    Complete lesson: Count words

--- main.py
+++ main.py
@@ -2,8 +2,14 @@ def get_book_text(path_to_file):
     with open(path_to_file,) as f:
         return f.read()
 
+def get_book_words(input):
+    total_words = input.split()
+    return len(total_words)
+
 def main():
     book_path = "books/frankenstein.txt"
-    print(get_book_text(book_path))
+    book_string = get_book_text(book_path)
+    num_words = get_book_words(book_string)
+    print(f"{num_words} words found in the document")
 
 main()
